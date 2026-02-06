414eed68d71f4192e3c653e819203d5a3fe9656a
Ishan Khire <ishankhire@gmail.com>
2026-02-06 03:55:01 -0500

    Copilot fix

--- scripts/migrate-json-to-db.ts
+++ scripts/migrate-json-to-db.ts
@@ -5,7 +5,7 @@
  */
 
 import "dotenv/config";
-import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 import * as fs from "fs";
